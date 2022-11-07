c955e7f0fa437593a04f2d30c445461789b302ee
AlyXom <109608998+AlyXom@users.noreply.github.com>
2022-11-07 06:15:52 -0300

    Add files via upload

--- Responsive/script.js
+++ Responsive/script.js
@@ -1,3 +1,19 @@
-const button = document.querySelector('.botao')
+let button = document.querySelector('.botao')
+const body = document.querySelector('main')
 
-console.log(button)
+button.addEventListener('click', cross)
+body.addEventListener('click', menu)
+
+function cross() {
+    if (button.src="menu-burger.svg") {
+        button.src="cross.svg"
+        console.log(button.src)
+    }
+} 
+
+function menu() {
+    if (button.src) {
+        button.src="menu-burger.svg"
+        console.log(button.src)
+    }
+}
